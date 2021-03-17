0876901030ac16bd97ff484cc9fae4d9c28c3459
Rop Gonggrijp <rop@gonggri.jp>
2021-03-17 13:54:29 +0100

    updates to command line options and readme

--- README.md
+++ README.md
@@ -4,27 +4,27 @@
 [![PyPI pyversions](https://img.shields.io/pypi/pyversions/klipz.svg)](https://pypi.python.org/pypi/wrtview/)
 [![MIT license](https://img.shields.io/badge/License-MIT-blue.svg)](https://github.com/ropg/wrtview/blob/master/LICENSE)
 
-`wrtview` compactly displays data about hosts on a network connected to an OpenWRT router by combining data from various files and the output of commands on the OpenWRT. It is typically ran on a computer connected to an openwrt-supplied network, not on the OpenWRT device itself.
+`wrtview` compactly displays data about hosts on a network centered around an OpenWRT router by combining data from various files and the output of commands on the OpenWRT. It is typically ran on a computer connected to a network that is built around an OpenWRT device, not on the OpenWRT device itself.
 
 ## Installation
 
 wrtview is a python package on [PyPI](https://pypi.org/project/wrtview). If you have python 3 installing is easy: 
 
-```bash
+```
 python -m pip install wrtview
 ```
 
 Alternatively, you can install the latest development version directly from this github repository:
 
-```bash
+```
 python -m pip install git+https://github.com/ropg/wrtview.git
 ```
 
 ## Setting up the access point for pubkey ssh access
 
-For wrtview to work, you will want to set up ssh to provide pubkey (passwordless) access to your openwrt device. If you have a Mac or a linux machine, this is done by entering ...
+For wrtview to work, you will need to set up ssh to provide pubkey (passwordless) access to your openwrt device. If you have a Mac or a linux machine, this is done by entering ...
 
-```bash
+```
 scp ~/.ssh/id_rsa.pub root@<router>:/etc/dropbear/authorized_keys
 ```
 
@@ -32,43 +32,89 @@ scp ~/.ssh/id_rsa.pub root@<router>:/etc/dropbear/authorized_keys
 
 ## Using wrtview
 
-If your router is at 192.168.1.1, all you need to do is type `wrtview` and you'll see output like this:
+If your router is at 192.168.1.1, all you need to do is enter `wrtview` and you'll see output like this:
 
-```text
+```
+Network 'lan' on 192.168.1.1:
+
   H  192.168.1.1   openwrt         
 ADHE 192.168.1.100 MacbookPro        F0:18:98:36:06:73  Apple, Inc.             wlan1 493.286Mbps
 ADHE 192.168.1.101 MacbookPro-wired  00:50:B6:98:C4:29  GOOD WAY IND. CO., LTD
 ADHE 192.168.1.105 iPad              26:5A:90:A8:52:73  locally administered    wlan1 380.493Mbps
-ADHE 192.168.1.130 Veraphone         EA:24:8C:29:DA:12  locally administered    wlan1 429.656Mbps
-A    192.168.1.151                   76:2A:A6:21:85:EC  locally administered       
+ADHE 192.168.1.130 JessicaPhone      EA:24:8C:29:DA:12  locally administered    wlan1 429.656Mbps
+AD   192.168.1.151                   76:2A:A6:21:85:EC  locally administered       
 ADHE 192.168.1.160 OldMacbookPro     20:C9:D0:84:02:D6  Apple, Inc.             wlan0 120.208Mbps
   HE 192.168.1.182 MacookAir         D0:E1:40:91:88:1E  Apple, Inc.                
 ADHE 192.168.1.188 iPhone-John       2E:73:1F:31:B0:1D  locally administered    wlan1 456.389Mbps
 ADHE 192.168.1.200 lights-gw         00:17:88:26:0A:26  Philips Lighting BV        
 ADHE 192.168.1.201 tv                10:4F:A8:03:00:8C  Sony Corporation           
 ADHE 192.168.1.212 printer           3C:2A:F4:42:24:A2  Brother Industries, LT     
-A    192.168.1.228                   2E:73:1F:31:B0:1D  locally administered    wlan1 456.389Mbps
+AD   192.168.1.228                   2E:73:1F:31:B0:1D  locally administered    wlan1 456.389Mbps
 ADHE 192.168.1.254 switch            00:1F:28:E2:66:82  HPN Supply Chain
 ```
 
-As you can see, this network has a router called 'openwrt'. By default, all the hosts on the 'lan' network om the router are displayed. In the first column you can see whether a host is in the router's ARP table (`A`), whether it was given a DHCP lease (`D`) and whether it is in the hosts (`H`) and ethers (`E`) files. Then there's the host's IP-address and name (the latter either from hosts, ethers or DHCP lease). After that there's the MAC-address and the manufacturer from the vendor database. Then if the MAC-address is found in the output of `iw <interface> station dump` for either wlan0 or wlan1, that is displayed with the expected throughput.
+As you can see, this network has a router called 'openwrt'. By default, all the hosts on the 'lan' network on the router are displayed. In the first column you can see whether a host is in the router's ARP table (`A`), whether it was given a DHCP lease (`D`) and whether it is in the hosts (`H`) and ethers (`E`) files. Then there's the host's IP-address and name (the latter either from hosts, ethers or DHCP lease). After that there's the MAC-address and the manufacturer from the vendor database.
 
-> As you can see the wlan0 interface is slower than the wlan1, probably because the former is 2.4 GHz and the latter is on 5 GHz. Also, the Apple mobile products randomize their MAC-addresses by default to prevent tracking, so they show up as 'locally administered', meaning they invented their own MAC-address.
+Then if the MAC-address is found in the output of `iw <interface> station dump` for either wlan0 or wlan1, that interface is displayed with the expected throughput. You can merge in data from other access points that are serving the same network elsewere in the building, see below for details.
+
+> As you can see the wlan0 interface is slower than the wlan1, because in this case the former is on 2.4 GHz and the latter is on 5 GHz. Also, Apple handheld products randomize their MAC-addresses by default to prevent tracking, so they show up as 'locally administered', meaning they invented their own MAC-address. The Macbook Air is not on, and the stations with addresses ending in 151 and 228 have connected, but they are not in the hosts or ethers files. 
 
 ## Command line options
 
 `--network`, `-n`
 
-By default, wrtview will display clients in the 'lan' network on the openwrt, but you can set a different network here.
+By default, wrtview will display clients in the 'lan' network on the openwrt, but you can set a different network here. To specify multiple networks, separate them with commas, without any added spaces. They will be listed one after the other with their own headers.
 
 &nbsp;
 
 `--wireless`, `-w`
 
-By default, wrtview checks for clients on 'wlan0' and 'wlan1'. but you can specify wireless interfaces that you would like to check for clients on. You can specify multiple wireless interfaces, in the format `<interface>[@<host>][:<alias>]`. The hostname part allows you to check for clients on a different OpenWRT that may be serving a different part of a building. So for instance:
+By default, wrtview checks for clients on 'wlan0' and 'wlan1'. But you can specify any set of wireless interfaces that you would like to check for clients on, separating them with commas without any added spaces. Interfaces should be provided in the format `<interface>[@<host>][:<alias>]`. The hostname part allows you to check for clients on a different OpenWRT that may be serving a different part of a building. So for instance:
 
-```text
-wrtview -w wlan0:S2 -w wlan1:S5 -w wlan0@192.168.0.4:N2 -w wlan1@192.168.0.4:N5 192.168.0.2
+```
+wrtview -w wlan0:S2,wlan1:S5,wlan0@192.168.0.4:N2,wlan1@192.168.0.4:N5 192.168.0.2
 ```
 
-will cause wrtview to connect to 192.168.1.2 for all the data, and show the 'wlan0' and 'wlan1' interfaces named as 'S2' and 'S5' respectively. Addtionally, this will cause wrtview to connect to 192.168.0.4 for data on clients to its 'wlan0' and 'wlan1' interfaces, marking their wifi connections 'N2' and 'N5' respectively in the output.
+will cause wrtview to connect to 192.168.0.2 for the DHCP, hosts, ethers and ARP information, and show the 'wlan0' and 'wlan1' interfaces on that system as 'S2' and 'S5' respectively. Addtionally, this will cause wrtview to connect to 192.168.0.4 for data on clients to its 'wlan0' and 'wlan1' interfaces, marking their wifi connections 'N2' and 'N5' respectively in the output.
+
+&nbsp;
+
+`--leases`, `--hosts`, `--ethers`
+
+These options can be used to specify alternative location for the DHCP leases file, the hosts file and the ethers files. The defaults should be fine though.
+
+&nbsp;
+
+`--format`, `-f`
+
+By default, wrtview prints output using the built-in default format string:
+
+```
+{arp}{dhcp}{hosts}{ethers} {ip:13.13} {name:17.17} {mac:17.17} {vendor:22.22}  {wifi alias} {wifi expected throughput}
+```
+
+This first four fields in this format string are formatted to either hold a space or an A, D, H or E respectively. The fields with the numbers after them are padded and cut at the specified length so that the out lines up nicely. Apert from the ones in the default format, you can use `wifi` (either 'W' or ' '), `clientID`, `wifi ap host`, `wifi ap interface` and every field from the output of `iw station dump <interface>`, prepended with `wifi `.
+
+&nbsp;
+
+`--sort`, `-s`
+
+By default, the output is sorted by IP-address. Use this option to specify any field name from above to sort on. For example:
+
+```
+wrtview -s name 192.168.0.2
+```
+
+will sort the output on hostname.
+
+&nbsp;
+
+`--no-ghosts`
+
+By default, wrtview will print any wifi stations that were found associated with probed wireless interfaces (such as 'wlan1') that were not part of any logical networks (such as 'lan') that you indicated. `--no-ghosts` suppresses this behaviour.
+
+&nbsp;
+
+`--no-header`
+
+Suppresses headers like `Network 'lan' on 192.168.1.1:`
--- wrtview/wrtview.py
+++ wrtview/wrtview.py
@@ -3,13 +3,17 @@
 import os, subprocess, re, ipaddress, sys, argparse, pkg_resources
 
 
+hosts = []
+stations = []
+
 def main():
+    global hosts
+    global stations
     default_format = "{arp}{dhcp}{hosts}{ethers} {ip:13.13} {name:17.17} {mac:17.17} " + \
                      "{vendor:22.22}  {wifi alias} {wifi expected throughput}"
 
     whitespace = re.compile('\s+')
-    hosts = {}
-    stations = []
+
 
     # Parse command line arguments
     parser = argparse.ArgumentParser()
@@ -17,15 +21,24 @@ def main():
     parser.add_argument('--hosts', default='/etc/hosts', metavar='<hosts-file>')
     parser.add_argument('--ethers', default='/etc/ethers', metavar='<ethers-file>')
     parser.add_argument('--network', '-n', default='lan', metavar='<interface>')
-    parser.add_argument('--wireless', '-w', nargs='*', default=['wlan0', 'wlan1'],
+    parser.add_argument('--wireless', '-w', default='wlan0,wlan1',
                         metavar='<interface>[@<host>][:<alias>]')
     parser.add_argument('--format', '-f', dest='format_str', default=default_format,
-                        metavar='<format string>')
-    parser.add_argument('host', nargs='?', default="192.168.1.1", metavar='<name or ip>')
+                        metavar='"<format string>"')
+    parser.add_argument('--sort', '-s', nargs=1, default='ip_as_int',
+                        metavar='<sort key>')
+    parser.add_argument('--no-ghosts', dest='no_ghosts', action='store_true')
+    parser.add_argument('--no-header', dest='no_header', action='store_true')
+    parser.add_argument('host', nargs='?', metavar='<name or ip>', default='192.168.1.1')
     args = parser.parse_args()
 
-    net_addr = get_output(args.host, 'uci get network.' + args.network + '.ipaddr')
-    net_mask = get_output(args.host, 'uci get network.' + args.network + '.netmask')
+    networks = []
+    for network in args.network.split(','):
+        addr = get_output(args.host, 'uci get network.' + network + '.ipaddr')
+        mask = get_output(args.host, 'uci get network.' + network + '.netmask')
+        if addr == '' or mask == '':
+            raise Exception("Network not found: " + network)
+        networks.append({'name': network, 'addr': addr, 'mask': mask})
 
     # Read vendor database
     vendor_re = re.compile('^([0-9A-F]+)\s+(.*?)$')
@@ -35,89 +48,98 @@ def main():
         s = vendor_re.search(line)
         if s:
             vendors[s.group(1)] = s.group(2)
+    # Once in memory is enough...
+    del vendorsoutput
+
+    # get all the global data we need from the target
+    leaseoutput = get_output(args.host, 'cat ' + args.leases)
+    hostsoutput = get_output(args.host, 'cat ' + args.hosts)
+    ethersoutput = get_output(args.host, 'cat ' + args.ethers)
+    arpoutput = get_output(args.host, 'ip -4 neigh')
 
     # Get the wireless station data
-    for w in args.wireless:
-        e = w.split(":", 2)
-        part1 = alias = e[0]
-        if len(e) == 2:
-            alias = e[1]
-        e = part1.split('@', 2)
-        iface = e[0]
-        if len(e) == 2:
-            whost = e[1]
-        else:
-            whost = args.host
-        stations.extend(wireless_stations(whost, iface, alias))
+    station_re = re.compile('^Station ([0-9A-Fa-f:]+)')
+    value_re = re.compile('\s+(.*?):\s*(.*)')
+    for w in args.wireless.split(','):
+        parts = w.split(":", 2)
+        alias = parts[1] if len(parts) == 2 else parts[0]
+        parts = parts[0].split('@', 2)
+        iface = parts[0]
+        whost = parts[1] if len(parts) == 2 else args.host
+        iwoutput = get_output(whost, 'iw ' + iface + ' station dump')
+        new_station = {}
+        for line in iwoutput.splitlines():
+            m = station_re.search(line)
+            if m:
+                if new_station != {}:
+                    stations.append(new_station)
+                    new_station = {}
+                new_station['mac']= m.group(1).upper()
+                new_station['wifi'] = 'W'
+                new_station['wifi ap host'] = whost
+                new_station['wifi ap interface'] = iface
+                new_station['wifi alias'] = alias
+            else:
+                m = value_re.search(line)
+                if m:
+                    new_station['wifi ' + m.group(1)] = m.group(2)
+        if new_station != {}:
+            stations.append(new_station)
 
     # DHCP
-    leaseoutput = get_output(args.host, 'cat /tmp/dhcp.leases')
     for line in leaseoutput.splitlines():
         expire, mac, ip, name, clientID = line.split(' ')
-        if in_same_subnet(ip, net_addr, net_mask):
-            ii = ip2int(ip)
-            hosts[ii] = {}
-            hosts[ii]['ip'] = ip
-            if name != "*": hosts[ii]['name'] = name
-            hosts[ii]['mac'] = mac.upper()
-            hosts[ii]['expire'] = int(expire)
-            if clientID != "*": hosts[ii]['clientID'] = clientID
-            hosts[ii]['dhcp'] = 'D'
+        host = find_host('ip', ip)
+        if name != "*": host['name'] = name
+        host['mac'] = mac.upper()
+        host['expire'] = int(expire)
+        if clientID != "*": host['clientID'] = clientID
+        host['dhcp'] = 'D'
 
     # hosts
-    hostsoutput = get_output(args.host, 'cat /etc/hosts')
     for line in hostsoutput.splitlines():
         line = re.sub('#.*', '', line.strip())
         if line == '': continue
         ip, name, *_ = whitespace.split(line)
-        if in_same_subnet(ip, net_addr, net_mask):
-            ii = ip2int(ip)
-            if not ii in hosts: hosts[ii] = {}
-            hosts[ii]['ip'] = ip
-            hosts[ii]['name'] = name
-            hosts[ii]['hosts'] = 'H';
+        host = find_host('ip', ip)
+        host['name'] = name
+        host['hosts'] = 'H';
 
     # ethers
-    ethersoutput = get_output(args.host, 'cat /etc/ethers')
     for line in ethersoutput.splitlines():
         line = re.sub('#.*', '', line.strip())
         if line == '': continue
         mac, name = whitespace.split(line)
-        for host in hosts.values():
+        for host in hosts:
             if host.get('mac') == mac.upper():
                 host['name'] == name
-                host['in_ethers'] = True
+                host['ethers'] = 'E'
             if host.get('name') == name:
                 host['mac'] = mac.upper()
                 host['ethers'] = 'E'
 
     # ARP
-    arpoutput = get_output(args.host, 'ip neigh')
     for line in arpoutput.splitlines():
         ip, _, _, _, mac, *_ = whitespace.split(line)
-        if in_same_subnet(ip, net_addr, net_mask):
-            ii = ip2int(ip)
-            if not ii in hosts: hosts[ii] = {}
-            hosts[ii]['ip'] = ip
-            hosts[ii]['mac'] = mac.upper()
-            hosts[ii]['arp'] = 'A'
+        host = find_host('ip', ip)
+        host['mac'] = mac.upper()
+        host['arp'] = 'A'
 
     # Merge in data from the wireless stations
     idx = 0
     for s in stations:
         mac = s['mac']
         found = False
-        for _, host in hosts.items():
+        for host in hosts:
             if mac == host.get('mac'):
                 found = True
                 host.update(s)
         if not found:
-            hosts[idx] = s
-            hosts[idx]['ip'] = '?'
-            idx += 1
+            s['ip'] = "?"
+            hosts.append(s)
 
     # Add in MAC vendor string from database
-    for host in hosts.values():
+    for host in hosts:
         mac = host.get('mac')
         if not mac: continue
         if "26AE".find(mac[1]) != -1:
@@ -127,21 +149,50 @@ def main():
             vendor = vendors.get(m[0:10], vendors.get(m[0:8], vendors.get(m[0:6], "unknown")))
             host['vendor'] = vendor
 
-    # Print output sorted by IP
-    for _, host in sorted(hosts.items()):
+    # Fix up missing keys
+    for host in hosts:
+        # Make sure sort key exists for all hosts
+        host[args.sort] = host.get(args.sort, ' ')
         # make sure the fields used in format_str are ' ' if they didn't exist
         for field in re.findall('\{(.*?)[\:\}]', args.format_str):
             host[field] = host.get(field, ' ');
-        print (args.format_str.format(**host))
+        # add numeric ip for sorting
+        host['ip_as_int'] = ip2int(host.get('ip', ''))
 
+    # Sort
+    sorted_hosts = sorted(hosts, key=lambda k: k[args.sort])
+
+    # Print output for each network, sorted by args.sort column
+    spacer = False
+    for network in networks:
+        if spacer: print ('\n')
+        spacer = True
+        if not args.no_header:
+            print (f"Network '{network['name']}' on {args.host}:\n")
+        for host in sorted_hosts:
+            if in_same_subnet(host.get('ip', ''), network['addr'], network['mask']):
+                print (args.format_str.format(**host))
+                host['printed'] = True
+
+    # Print the 'ghosts': wifi stations that were not found in the networks specified
+    if not args.no_ghosts:
+        header_printed = False
+        for host in sorted_hosts:
+            if host.get('wifi') and not host.get('printed'):
+                if not header_printed:
+                    if len(networks) == 1:
+                        print (f"\nWiFi Stations not in this network:\n")
+                    else:
+                        print (f"\nWiFi Stations not in these networks:\n")
+                    header_printed = True
+                print (args.format_str.format(**host))
 
 
 
 # return output from command at host as string
 def get_output(host, command):
     return subprocess.run(['ssh', 'root@' + host, command],
-                          stdout=subprocess.PIPE,
-                          timeout = 5
+                          capture_output = True, timeout = 5
                          ).stdout.decode('utf-8').strip()
 
 def ip2int(ip):
@@ -153,30 +204,15 @@ def ip2int(ip):
 def in_same_subnet(ip1, ip2, mask):
     return ip2int(ip1) & ip2int(mask) == ip2int(ip2) & ip2int(mask)
 
-# append data from one wireless interface to stations[]
-def wireless_stations(host, interface, alias):
-    stations = []
-    station_re = re.compile('^Station ([0-9A-Fa-f:]+)')
-    value_re = re.compile('\s+(.*?):\s*(.*)')
-    iwoutput = get_output(host, 'iw ' + interface + ' station dump')
-    s = {}
-    for line in iwoutput.splitlines():
-        m = station_re.search(line)
-        if m:
-            if s != {}:
-                stations.append(s)
-                s = {}
-            s['mac']= m.group(1).upper()
-            s['wifi ap host'] = host
-            s['wifi ap interface'] = interface
-            s['wifi alias'] = alias
-        else:
-            m = value_re.search(line)
-            if m:
-                s['wifi ' + m.group(1)] = m.group(2)
-    if s != {}:
-        stations.append(s)
-    return stations
+def find_host(k, v, add=True):
+    global hosts
+    for host in hosts:
+        if host[k] == v:
+            return host
+    if add:
+        new_item = { k: v }
+        hosts.append(new_item)
+        return hosts[-1]
 
 
 if __name__ == "__main__":
